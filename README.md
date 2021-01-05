f5de873056ba93a84ab6141d3bb67e3aa4dbe8c0
Hillevi Annfält <hillevi.annfalt@hotmail.com>
2021-01-05 17:54:53 +0100

    Färdig webbplats som använder API

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-# Webbplats skapad med Gulp, Browser-sync, Scss och Babel
+# Webbplats som läser in kurser via API
 
-En enkel webbplats bestående utav bara en startsida men som har utformats med hjälp av utvecklingsverktygen Gulp och Scss vilket verkligen har underlättat processen. Till detta moment har även Babel lagts till för att under utvecklingen kunna avända ES6 och sedan transpilera koden till ES5 så att koden fungerar som den ska i alla webbläsare.  
+En webbplats som med hjälp utav Fetch och Promises hämtar in och skriver ut kurser från Mittuniversitet via ett externt API. Det är även möjligt att själva lägga till eller ta bort kurser ur listan. 
 
-Har även testat att lägga in en klass som skriver ut i konsolen för att se så att transpilering fungerar. Den fyller ingen funktion mer än att visa att det fungerar. 
+Webbplatsen har skapats med HTML, CSS och JavaScript. För utvecklingen har SCSS och ES6 använts men för publicering har dessa anpassats till vanlig CSS och ES5 för att få stöd i webbläsare. Automatisering har gjorts med hjälp ut Gulp. 
 
-### Skrivet av **Hillevi Annfält** 2020-12-10
+Länk till färdig webbplats: 
+http://moment5.hilleviannfalt.se/
+
+### Skrivet av **Hillevi Annfält** 2020-12-18
--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,70 @@
 'use strict';
 
-//Showing and hiding the menue options
-let button = document.getElementById("navButton");
-let menu = document.getElementById("hiddenNav");
-        
-button.onclick = function() {
-    
-    if(menu.style.display === "block") {
-        menu.style.display ="none";
-    } 
-    else {
-        menu.style.display = "block";
-    }
-};
+//Variabler 
+const URL = "http://courses.hilleviannfalt.se";
 
-class Person {
-        constructor(firstName, lastName, age) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.age = age;
-        }
-        showInfo() {
-            console.log(`Hej ${this.firstName} ${this.lastName}. Du är ${this.age} år.`);
-        } 
-    }
-    let person = new Person("Hillevi", "Annfält", 31);
-    person.showInfo();
+let courseList = document.getElementById("courseList");
+let codeInput = document.getElementById("courseCode");
+let nameInput = document.getElementById("courseName");
+let progressionInput = document.getElementById("progression");
+let syllabusInput = document.getElementById("syllabus");
+let addCourseButton = document.getElementById("addCourse");
+
+//Event hanterare
+window.addEventListener("load", getCourses);
+
+addCourseButton.addEventListener("click", function(event){
+    event.preventDefault();
+    addNewCourse();
+});
+
+//Läser in kurser
+function getCourses() {
+    courseList.innerHTML = "";
+
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+        courseList.innerHTML = "<tr><th>Kurskod</th> <th>Kursnamn</th> <th>Progression</th> <th>Kursplan</th> <th>Ta bort</th></tr>";
+        data.forEach(course => {
+            courseList.innerHTML +=`<tr><td> ${course.CourseCode}</td> <td>${course.CourseName}</td> <td> ${course.Progression}</td> <td><a href='${course.CourseSyllabus}'>Kursplan</a></td> <td><button id="${course.ID}" class="remove" onClick="deleteCourse(${course.ID})">X</button></td></tr>`;
+        })   
+    })
+}
+//Tar bort en kurs
+function deleteCourse(id) {
+    fetch(URL + "?ID=" + id, {
+        method: "DELETE",
+    })
+    .then(res => res.json())
+    .then(data => {
+        getCourses();
+    })
+    .catch(error => {
+        console.log(`Fel: ${error}`);
+    })
+}
+
+//Lägger till ny kurs
+function addNewCourse() {
+
+    let code = codeInput.value;
+    let name = nameInput.value;
+    let progression = progressionInput.value;
+    let syllabus = syllabusInput.value;
+    
+    //Skapar nytt objekt med inskickad data
+    let course =  {"code": code, "name": name, "progression": progression, "courseSyllabus": syllabus };
+
+    fetch(URL, {
+        method: "POST",
+        body: JSON.stringify(course),
+    })
+    .then(res => res.json())
+    .then( data => {
+        location.reload();
+    })
+    .catch(error=> {
+        console(`Fel: ${error}`);
+    })    
+}
